744532d4863da8f04b59300cdf88a781e3c5472d
Abhishek047 <abhi.shukla0047@gmail.com>
2025-05-21 19:09:46 +0530

    Updated the main app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import NextBundleAnalyzer from "@next/bundle-analyzer";
 
+const withBundleAnalyzer = NextBundleAnalyzer({
+    enabled: process.env.ANALYZE === "true",
+});
 const nextConfig: NextConfig = {
-  /* config options here */
+    turbopack: {},
+    images: {
+        dangerouslyAllowSVG: true,
+    },
+    distDir: process.env.OUTPUT_DIR || ".next",
 };
 
-export default nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
