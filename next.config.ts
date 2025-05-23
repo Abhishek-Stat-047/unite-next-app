f0d38fe1c80da01a5de30affb507cc25da0cbab4
Abhishek047 <abhi.shukla0047@gmail.com>
2025-05-23 10:29:47 +0530

    Added docker file

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,17 @@
 import type { NextConfig } from "next";
+
 import NextBundleAnalyzer from "@next/bundle-analyzer";
 
 const withBundleAnalyzer = NextBundleAnalyzer({
     enabled: process.env.ANALYZE === "true",
 });
+const isDocker = process.env.BUILD_ENV === "docker";
 const nextConfig: NextConfig = {
     turbopack: {},
     images: {
         dangerouslyAllowSVG: true,
     },
+    output: isDocker ? "standalone" : undefined,
     distDir: process.env.OUTPUT_DIR || ".next",
 };
 
